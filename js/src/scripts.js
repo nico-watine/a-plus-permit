80e59cfabc994b0c27c05eaf66cb7883d8abbfc8
Nico Watine <nico@studioncreations.com>
2019-12-29 12:33:24 -0600

    Duplicate skrollr source file into js directory

--- js/src/scripts.js
+++ js/src/scripts.js
@@ -1,5 +1,5 @@
 // @codekit-prepend "jquery.reveal.js";
-// @codekit-prepend "/bower_components/skrollr/dist/skrollr.min.js";
+// @codekit-prepend "skrollr.min.js";
 
 skrollr.init({
 	smoothScrolling: true
